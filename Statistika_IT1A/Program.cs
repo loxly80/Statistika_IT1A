c21e7a14219c72789415d279375d080071fafab5
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-23 08:39:00 +0100

    Add project files.

---
+++ Statistika_IT1A/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Statistika_IT1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(false);   //Zachycení stisknuté klávesy
+                if(keyInfo.Key == ConsoleKey.Enter)
+                {
+                    //když uživatel zmáčkne Enter
+
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
